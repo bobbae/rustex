8db2396a41ebe3829c7ec2996d5770151a40f88e
bob bae <bob.bae@gmail.com>
2021-01-26 03:52:30 +0000

    github

--- httpget1/src/main.rs
+++ httpget1/src/main.rs
@@ -1,11 +1,13 @@
 use error_chain::error_chain;
 use std::io::Read;
+
 error_chain! {
     foreign_links {
         Io(std::io::Error);
         HttpRequest(reqwest::Error);
     }
 }
+
 fn main() -> Result<()> {
     let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
     let mut body = String::new();
